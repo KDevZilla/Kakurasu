d061ef50699b74d0fefd8576da387ff9ae3dc74d
Krirk Srithaweewath <Krirk.srithaweewath@finexusgroup.com>
2024-02-04 15:50:05 +0800

    Update

--- Kakurasu/Program.cs
+++ Kakurasu/Program.cs
@@ -16,7 +16,7 @@ namespace Kakurasu
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
+            Application.Run(new FormKakurasu());
         }
     }
 }
--- Kakurasu/Resource1.Designer.cs
+++ Kakurasu/Resource1.Designer.cs
@@ -8,7 +8,7 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
-namespace Kakurasu.Properties {
+namespace Kakurasu {
     using System;
     
     
@@ -22,14 +22,14 @@ namespace Kakurasu.Properties {
     [global::System.CodeDom.Compiler.GeneratedCodeAttribute("System.Resources.Tools.StronglyTypedResourceBuilder", "15.0.0.0")]
     [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
     [global::System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
-    internal class Resources {
+    internal class Resource1 {
         
         private static global::System.Resources.ResourceManager resourceMan;
         
         private static global::System.Globalization.CultureInfo resourceCulture;
         
         [global::System.Diagnostics.CodeAnalysis.SuppressMessageAttribute("Microsoft.Performance", "CA1811:AvoidUncalledPrivateCode")]
-        internal Resources() {
+        internal Resource1() {
         }
         
         /// <summary>
@@ -39,7 +39,7 @@ namespace Kakurasu.Properties {
         internal static global::System.Resources.ResourceManager ResourceManager {
             get {
                 if (object.ReferenceEquals(resourceMan, null)) {
-                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("Kakurasu.Properties.Resources", typeof(Resources).Assembly);
+                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("Kakurasu.Resource1", typeof(Resource1).Assembly);
                     resourceMan = temp;
                 }
                 return resourceMan;
@@ -67,16 +67,6 @@ namespace Kakurasu.Properties {
             get {
                 object obj = ResourceManager.GetObject("Kakurasu_32", resourceCulture);
                 return ((System.Drawing.Icon)(obj));
-            }
-        }
-        
-        /// <summary>
-        ///   Looks up a localized resource of type System.Drawing.Bitmap.
-        /// </summary>
-        internal static System.Drawing.Bitmap Kakurasu_Screen_021 {
-            get {
-                object obj = ResourceManager.GetObject("Kakurasu_Screen_021", resourceCulture);
-                return ((System.Drawing.Bitmap)(obj));
             }
         }
     }
