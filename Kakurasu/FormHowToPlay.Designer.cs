a497419181670d6657e7bce4a58e74534d0f9f17
Krirk Srithaweewath <Krirk.srithaweewath@finexusgroup.com>
2024-02-05 00:42:00 +0800

    Update

--- Kakurasu/FormHowToPlay.Designer.cs
+++ Kakurasu/FormHowToPlay.Designer.cs
@@ -50,7 +50,7 @@
             this.label1.AutoSize = true;
             this.label1.Location = new System.Drawing.Point(387, 9);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(613, 399);
+            this.label1.Size = new System.Drawing.Size(644, 420);
             this.label1.TabIndex = 5;
             this.label1.Text = resources.GetString("label1.Text");
             // 
